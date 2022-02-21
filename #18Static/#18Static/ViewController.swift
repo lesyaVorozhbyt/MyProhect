66495203377f33aecf5da639102558ea8fec7e77
lvorozhbyt <lesya.vorozhbyt@gmail.com>
2022-02-21 18:55:53 +0100

    Add Static xcode project.

---
+++ #18Static/#18Static/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  #18Static
+//
+//  Created by Lesia Vorozhbyt on 21.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
